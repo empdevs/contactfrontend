2ab41abd6ce84d6100c0bd41d26faf8de009aa1d
haekalarif <haekal.rozikin@kitameraki.com>
2024-01-06 16:51:31 +0700

    chore: Disabled login with telegram

--- src/Components/TelegramButton.tsx
+++ src/Components/TelegramButton.tsx
@@ -21,51 +21,51 @@ declare global {
 }
 
 const TelegramButton = (props: ITelegramButton) => {
-    const instanceRef: any = useRef(null);
+    // const instanceRef: any = useRef(null);
 
-    useEffect(() => {
-        window.TelegramLoginWidget = {
-            dataOnauth: (user: any) => props.dataOnauth(user),
-        };
-        const script = document.createElement("script");
-        script.src = `https://telegram.org/js/telegram-widget.js?${props.widgetVersion}`;
-        script.setAttribute("data-telegram-login", props.botName);
-        script.setAttribute("data-size", props.buttonSize);
-        if (props.cornerRadius !== undefined) {
-            script.setAttribute("data-radius", props.cornerRadius);
-        }
-        script.setAttribute("data-request-access", props.requestAccess);
-        script.setAttribute("data-userpic", props.usePic);
-        script.setAttribute("data-lang", props.lang);
+    // useEffect(() => {
+    //     window.TelegramLoginWidget = {
+    //         dataOnauth: (user: any) => props.dataOnauth(user),
+    //     };
+    //     const script = document.createElement("script");
+    //     script.src = `https://telegram.org/js/telegram-widget.js?${props.widgetVersion}`;
+    //     script.setAttribute("data-telegram-login", props.botName);
+    //     script.setAttribute("data-size", props.buttonSize);
+    //     if (props.cornerRadius !== undefined) {
+    //         script.setAttribute("data-radius", props.cornerRadius);
+    //     }
+    //     script.setAttribute("data-request-access", props.requestAccess);
+    //     script.setAttribute("data-userpic", props.usePic);
+    //     script.setAttribute("data-lang", props.lang);
 
-        if (props.dataAuthUrl !== undefined) script.setAttribute("data-auth-url", props.dataAuthUrl);
-        else script.setAttribute("data-onauth", "TelegramLoginWidget.dataOnauth(user)");
+    //     if (props.dataAuthUrl !== undefined) script.setAttribute("data-auth-url", props.dataAuthUrl);
+    //     else script.setAttribute("data-onauth", "TelegramLoginWidget.dataOnauth(user)");
 
-        script.async = true;
-        instanceRef.current.appendChild(script);
+    //     script.async = true;
+    //     instanceRef.current.appendChild(script);
         
-    }, [
-        props.botName,
-        props.buttonSize,
-        props.cornerRadius,
-        props.requestAccess,
-        props.usePic,
-        props.dataOnauth,
-        props.dataAuthUrl,
-        props.lang,
-        props.widgetVersion,
-    ]);
+    // }, [
+    //     props.botName,
+    //     props.buttonSize,
+    //     props.cornerRadius,
+    //     props.requestAccess,
+    //     props.usePic,
+    //     props.dataOnauth,
+    //     props.dataAuthUrl,
+    //     props.lang,
+    //     props.widgetVersion,
+    // ]);
 
     return (
         <>
-            <div
+            {/* <div
                 className={props.className}
                 ref={(component) => {
                     instanceRef.current = component;
                 }}
             >
                 {props.children}
-            </div>
+            </div> */}
         </>
     );
 };
