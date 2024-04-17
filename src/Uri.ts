0101c3607b99594b757c6360c6c3292a9fe44cfc
haekalarif <haekal.rozikin@kitameraki.com>
2024-04-17 13:48:25 +0700

    build: We use VITE for development server & build for production

--- src/Uri.ts
+++ src/Uri.ts
@@ -1,10 +1,10 @@
 class Uri {
-    public static get rootUri(): string { return process.env.REACT_APP_API_ROOT! }
-    public static get auth(): string { return process.env.REACT_APP_API_AUTH! }
-    public static get getContact(): string { return process.env.REACT_APP_API_GET_CONTACT! }
-    public static get insertContact(): string { return process.env.REACT_APP_API_INSERT_CONTACT! }
-    public static get deleteContact(): string { return process.env.REACT_APP_API_DELETE_CONTACT! }
-    public static get patchContact(): string { return process.env.REACT_APP_API_PATCH_CONTACT! }
-    public static get refreshToken(): string { return process.env.REACT_APP_API_REFRESH_TOKEN! }
+    public static get rootUri(): string { return import.meta.env["VITE_APP_API_ROOT"] }
+    public static get auth(): string { return import.meta.env["VITE_APP_API_AUTH"] }
+    public static get getContact(): string { return import.meta.env["VITE_APP_API_GET_CONTACT"] }
+    public static get insertContact(): string { return import.meta.env["VITE_APP_API_INSERT_CONTACT"] }
+    public static get deleteContact(): string { return import.meta.env["VITE_APP_API_DELETE_CONTACT"] }
+    public static get patchContact(): string { return import.meta.env["VITE_APP_API_PATCH_CONTACT"] }
+    public static get refreshToken(): string { return import.meta.env["VITE_APP_API_REFRESH_TOKEN"] }
 };
 export default Uri;
